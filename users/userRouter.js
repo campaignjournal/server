3f486085526f86749f2d39e2d51b2df694971195
MMendes24 <mmbusiness1@protonmail.com>
2020-10-09 18:30:52 -0400

    User router fully operational. No middleware or auth yet.

--- users/userRouter.js
+++ users/userRouter.js
@@ -21,4 +21,50 @@ router.get("/:id", (req, res) => {
         .catch((err) => res.send(err))
 })
 
+router.post("/", (req, res) => {
+    const newUser = req.body
+    
+    Users.create(newUser)
+        .then(thenRes => {
+            res.status(201).json(newUser)
+        })
+        .catch(err => {
+            res.status(400).json({ errorMessage: "Incorrectly shaped data." })
+        })
+
+})
+
+router.put("/:id", (req, res) => {
+    const editedUser = req.body
+    const id = req.params.id
+
+    Users.update(id, editedUser)
+            .then(thenRes => {
+                if (thenRes) {
+                    res.status(204).json(editedUser)
+                } else {
+                    res.status(404).json({ errorMessage: "Record does not exist." })
+                }
+            })
+            .catch(err => {
+                res.status(500).json({ errorMessage: "Internal server error." })
+            })
+
+})
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id
+    Users.remove(id)
+        .then(thenRes => {
+            if (thenRes) {
+                res.status(200).json(thenRes)
+            } else {
+                res.status(404).json({ errorMessage: "Record does not exist." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "Internal server error." })
+        })
+})
+
 module.exports = router
