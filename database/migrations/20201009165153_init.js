84d44c17f4e7fa6bfa23282df3a9100715b49c03
MMendes24 <mmbusiness1@protonmail.com>
2020-10-09 17:32:17 -0400

    First migration created.

--- database/migrations/20201009165153_init.js
+++ database/migrations/20201009165153_init.js
@@ -1,7 +1,114 @@
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("users", (tbl) => {
+            tbl.increments()
+            tbl.string("username", 128).notNullable().unique().index()
+            tbl.string("password", 256).notNullable()
+            tbl.string("email", 256).notNullable().unique()
+        })
+        .createTable("campaigns", (tbl) => {
+            tbl.increments()
+            tbl.integer("user_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("users")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
 
-exports.down = function(knex) {
-  
+            tbl.string("name", 256)
+                .notNullable()
+                .unique()
+                .index()
+
+            tbl.string("desc", 256)
+        })
+        .createTable("countries", (tbl) => {
+            tbl.increments()
+            tbl.integer("campaign_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("campaigns")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+
+            tbl.string("ruler", 256).notNullable()
+            tbl.string("name", 256).notNullable()
+            tbl.string("founded", 256).notNullable()
+            tbl.string("description", 256).notNullable()
+
+        })
+
+        .createTable("characters", (tbl) => {
+            tbl.increments()
+            tbl.integer("campaign_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("campaign")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+
+            tbl.string("ancestry", 256).notNullable()
+            tbl.string("name", 256).notNullable()
+            tbl.string("level", 256).notNullable()
+            tbl.string("class", 256).notNullable()
+            tbl.string("description", 256).notNullable()
+
+        })
+        .createTable("world", (tbl) => {
+            tbl.increments()
+            tbl.integer("campaign_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("campaign")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+
+            tbl.string("name", 256).notNullable().unique().index()
+            tbl.string("desc", 256)
+        })
+
+        .createTable("religions", (tbl) => {
+            tbl.increments()
+            tbl.integer("world_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("world")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+
+            tbl.string("gods", 256).notNullable()
+            tbl.string("doctrines", 256).notNullable()
+            tbl.string("description", 256).notNullable()
+        })
+
+        .createTable("history", (tbl) => {
+            tbl.increments()
+            tbl.integer("world_id")
+                .unsigned()
+                .notNullable()
+                .references("id")
+                .inTable("world")
+                .onUpdate("CASCADE")
+                .onDelete("CASCADE");
+
+
+            tbl.string("name", 256)
+            tbl.string("date", 256).notNullable()
+            tbl.string("description", 256).notNullable()
+        })
+}
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists("history")
+        .dropTableIfExists("religions")
+        .dropTableIfExists("worlds")
+        .dropTableIfExists("characters")
+        .dropTableIfExists("countries")
+        .dropTableIfExists("campaigns")
+        .dropTableIfExists("users")
 };
