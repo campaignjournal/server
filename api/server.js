bbf25941c3029604e2ec9bd5e1eef3b3cb2011ff
MMendes24 <mmbusiness1@protonmail.com>
2020-10-09 18:01:01 -0400

    Server up and running for first time. Doesn't do anything.

---
+++ api/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config()
+
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+server.get("/", (req, res) => {
+    res.send({message: "up, check it out"})
+})
+
+module.exports = server
