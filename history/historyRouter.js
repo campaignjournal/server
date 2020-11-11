8e241012373fbafedc0e5bb5737d6e4c70332218
MMendes24 <mmbusiness1@protonmail.com>
2020-11-11 12:02:36 -0600

    You can now delete historical events. Last MVP endpoint completed.

--- history/historyRouter.js
+++ history/historyRouter.js
@@ -27,10 +27,10 @@ router.post("/:id/worlds/:worldid/history", (req, res) => {
     const newEvent = req.body
 
     History.createEvent(worldId, newEvent)
-    .then((history) => {
-        res.status(201).json({ data: history })
-    })
-    .catch((err) => res.send (err))
+        .then((history) => {
+            res.status(201).json({ data: history })
+        })
+        .catch((err) => res.send(err))
 })
 
 router.put("/:id/worlds/:worldid/history/:eventid", (req, res) => {
@@ -38,10 +38,20 @@ router.put("/:id/worlds/:worldid/history/:eventid", (req, res) => {
     const changes = req.body
 
     History.updateEvent(eventId, changes)
-    .then((history) => {
-        res.status(200).json({ data: history })
-    })
-    .catch((err) => res.send (err))
+        .then((history) => {
+            res.status(200).json({ data: history })
+        })
+        .catch((err) => res.send(err))
+})
+
+router.delete("/:id/worlds/:worldid/history/:eventid", (req, res) => {
+    const eventId = req.params.eventid
+
+    History.destroyEvent(eventId)
+        .then(() => {
+            res.status(200).json({ message: "Successfully deleted" })
+        })
+        .catch((err) => res.send(err))
 })
 
 module.exports = router
