e6a59629a381d08e44bc9e6e3d1405694e5036c5
MMendes24 <mmbusiness1@protonmail.com>
2020-11-11 14:56:48 -0600

    User router and userValidator completed.

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,68 +2,114 @@ const router = require("express").Router()
 const bcryptjs = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
-const Users = require('./UserModel')
+const Users = require("./UserModel")
+const userValidator = require("../middleware/userValidator")
 
 router.get("/", (req, res) => {
     Users.find()
         .then((users) => {
-            res.status(200).json({ data: users })
+            if (users) {
+                res.status(200).json({ data: users })
+            } else {
+                res.status(404).json({
+                    message: "No users have joined, yet!"
+                })
+            }
         })
-        .catch((err) => res.send(err))
+        .catch((err) => {
+            res.status(500).json({
+                errorMessage: "Internal server error."
+            })
+        })
 })
 
 router.get("/:id", (req, res) => {
     const id = req.params.id
     Users.findById(id)
-        .then((users) => {
-            res.status(200).json({ data: users })
+        .then((user) => {
+            if (user) {
+                res.status(200).json({ data: user })
+            } else {
+                res.status(404).json({
+                    message: "Record does not exist."
+                })
+            }
         })
-        .catch((err) => res.send(err))
+        .catch((err) => {
+            res.status(500).json({
+                errorMessage: "Internal server error."
+            })
+        })
 })
 
 router.post("/", (req, res) => {
     const newUser = req.body
-    
-    Users.create(newUser)
-        .then(thenRes => {
-            res.status(201).json(newUser)
-        })
-        .catch(err => {
-            res.status(400).json({ errorMessage: "Incorrectly shaped data." })
-        })
+    const legitUser = userValidator(newUser)
 
+    if (legitUser) {
+        Users.create(newUser)
+            .then(thenRes => {
+                res.status(201).json({
+                    data: newUser
+                })
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    errorMessage: "Internal server error."
+                })
+            })
+    } else {
+        res.status(400).json({
+            errorMessage: "Users have the following required attributes: username, password, and email. Username and password MUST be unique."
+        })
+    }
 })
 
 router.put("/:id", (req, res) => {
     const editedUser = req.body
     const id = req.params.id
+    const legitUser = userValidator(editedUser)
 
-    Users.update(id, editedUser)
+    if (legitUser) {
+        Users.update(id, editedUser)
             .then(thenRes => {
                 if (thenRes) {
-                    res.status(204).json(editedUser)
+                    res.status(200).json(editedUser)
                 } else {
                     res.status(404).json({ errorMessage: "Record does not exist." })
                 }
             })
-            .catch(err => {
-                res.status(500).json({ errorMessage: "Internal server error." })
+            .catch((err) => {
+                res.status(500).json({
+                    errorMessage: "Internal server error."
+                })
             })
-
+    } else {
+        res.status(400).json({
+            errorMessage: "Users have the following required attributes: username, password, and email. Username and password MUST be unique."
+        })
+    }
 })
 
 router.delete("/:id", (req, res) => {
     const id = req.params.id
+
     Users.remove(id)
         .then(thenRes => {
             if (thenRes) {
-                res.status(200).json(thenRes)
+                res.status(200).json({
+                    message: "User successfully deleted."
+                })
             } else {
-                res.status(404).json({ errorMessage: "Record does not exist." })
+                res.status(404).json({
+                    errorMessage: "Record does not exist."
+                })
             }
         })
-        .catch(err => {
-            res.status(500).json({ errorMessage: "Internal server error." })
+        .catch((err) => {
+            res.status(500).json({
+                errorMessage: "Internal server error."
+            })
         })
 })
 
