1303d525e9c7d694a74fc0a62dfeaf9bd8074396
MMendes24 <mmbusiness1@protonmail.com>
2020-11-11 17:02:25 -0600

    Separate testing database created.

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -4,6 +4,8 @@ const knex = require("knex")
 
 const knexfile = require("../knexfile.js")
 
-const environment = process.env.NODE_ENV || "development"
+// const environment = process.env.NODE_ENV || "development"
+
+const environment = "testing"
 
 module.exports = knex(knexfile[environment])
