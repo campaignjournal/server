d98c8ffa14287a8b2f2b11b815be64e24d1d7062
MMendes24 <mmbusiness1@protonmail.com>
2020-11-12 14:14:40 -0600

    Bugfixing on main

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ require("dotenv").config()
 
 const server = require("./api/server.js")
 
-const port = process.env.PORT || 5000
-server.listen(port, () => {
-    console.log(`\n** Running on port ${port} **\n`)
+server.listen(process.env.PORT || 5000, () => {
+    console.log("Up and running!")
 })
 
