b2c26c7b396265744c989f706fe13f39ce28cc87
MMendes24 <mmbusiness1@protonmail.com>
2020-11-12 17:04:56 -0600

    Eliminated staging environment

--- knexfile.js
+++ knexfile.js
@@ -15,23 +15,7 @@ module.exports = {
       directory: "./database/seeds",
     },
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
+  
   production: {
     client: "sqlite3",
     useNullAsDefault: true,
