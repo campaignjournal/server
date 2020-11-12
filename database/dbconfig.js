e97d04ea8e26540d9ab1d43facec4db425904885
MMendes24 <mmbusiness1@protonmail.com>
2020-11-12 14:07:46 -0600

    Switched the environment variable to be configurable by Heroku

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -4,8 +4,8 @@ const knex = require("knex")
 
 const knexfile = require("../knexfile.js")
 
-// const environment = process.env.NODE_ENV || "development"
+const environment = process.env.NODE_ENV || "development"
 
-const environment = "testing"
+// const environment = "testing"
 
 module.exports = knex(knexfile[environment])
